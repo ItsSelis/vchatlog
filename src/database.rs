86cc0540d7dffc7806524791cc1f3902fc32d68f
ItsSelis <12716288+ItsSelis@users.noreply.github.com>
2025-02-25 15:50:44 +0100

    fixed typo in the warning

--- src/database.rs
+++ src/database.rs
@@ -13,7 +13,7 @@ static SQL_CONNECTION: Lazy<Arc<Pool>> = Lazy::new(|| {
     ));
 
     // Clean up old stuff
-    fs::remove_dir_all("data/chatlogs").unwrap_or_else(|e| warn!("Error while trying to create temporary chatlogs directory: {e}"));
+    fs::remove_dir_all("data/chatlogs").unwrap_or_else(|e| warn!("Error while trying to delete temporary chatlogs directory: {e}"));
 
     let db_user = std::env::var("DB_USER").expect("environment variable DB_USER");
     let db_pass = std::env::var("DB_PASS").expect("environment variable DB_PASS");
